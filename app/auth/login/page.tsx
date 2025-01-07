cdc011ab26ba2d81c78cf83cf44851e3d396b4be
samir20-23 <germanysamir1@gmail.com>
2025-01-07 00:57:08 +0000

    'nextjsbackg'

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+import { useState } from "react";
+import axios from "axios";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    const res = await axios.post("/api/auth/login", { email, password });
+    console.log(res.data);
+  };
+
+  return (
+    <div>
+      <input value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
+      <input value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+}
